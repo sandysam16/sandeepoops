d4c49c7f33d42df71d777bc374f9fa4082c55bf5
sandysam16 <sandeepk136@gmail.com>
2019-12-16 11:41:32 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>Singup Form</title>
+ <link rel="stylesheet" href="style.css">
+ <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet"> 
+</head>
+<body>
+ 
+ <div class="container">
+  <div class="form">
+   <div class="form-section">
+    <form action="">
+     <div class="group">
+      <h3 class="heading">Create account</h3>
+     </div>
+     <div class="group">
+      <input type="text" name="name" class="control" placeholder="Enter Name...">
+     </div>
+     <div class="group">
+      <input type="email" name="name" class="control" placeholder="Enter Email..">
+     </div>
+     <div class="group">
+      <input type="password" name="name" class="control" placeholder="Create Password...">
+     </div>
+     <div class="group">
+      <input type="password" name="name" class="control" placeholder="Confirm Password...">
+     </div>
+     <div class="group m20">
+      <input type="submit" name="name" class="btn" value="Create account &rarr;">
+     </div>
+     <div class="group m20">
+      <a href="login.html" class="link">Already have an account ?</a>
+     </div>
+    </form>
+   </div>
+  </div>
+ </div>
+
+
+</body>
+</html>
